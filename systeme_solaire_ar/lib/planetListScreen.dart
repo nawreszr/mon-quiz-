ec07802c698f6cbeb824ad58f628a26df8e8e48a
nawreszr <nawreszarouki40@gmail.com>
2024-11-10 19:08:14 +0100

    first commit

--- systeme_solaire_ar/lib/planetListScreen.dart
+++ systeme_solaire_ar/lib/planetListScreen.dart
@@ -1,46 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:systeme_solaire_ar/planetDetailsScreen.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class PlanetListScreen extends StatelessWidget {
-  final List<Map<String, String>> planets = [
-    {
-      'name': 'Mercury',
-      'image': 'assets/mercury.png',
-      'description': 'Mercury is the smallest planet in our solar system...',
-    },
-    {
-      'name': 'Venus',
-      'image': 'assets/venus.png',
-      'description': 'Venus is the second planet from the sun...',
-    },
-  ];
+class PlanetListScreen extends StatefulWidget {
+  const PlanetListScreen({super.key});
+
+  @override
+  State<PlanetListScreen> createState() => _PlanetListScreenState();
+}
+
+class _PlanetListScreenState extends State<PlanetListScreen> {
+  List<Map<String, dynamic>> solarItems = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSolarData();
+  }
+
+  Future<void> fetchSolarData() async {
+    try {
+      final response = await http.get(Uri.parse('https://systeme-solaire-api.onrender.com/'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        setState(() {
+          solarItems = data.map((item) {
+            return {
+              'id': item['id'],
+              'image': item['image'],
+              'name': item['name'],
+              'description': item['description'],
+            };
+          }).toList();
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+        throw Exception('Failed to load solar data');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Solar System'),
+        title: const Text('النظام الشمسي'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: planets.length,
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : solarItems.isEmpty
+          ? const Center(child: Text('No solar data available.'))
+          : ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: solarItems.length,
         itemBuilder: (context, index) {
+          final item = solarItems[index];
           return Card(
             child: ListTile(
-              leading: Image.asset(planets[index]['image']!),
-              title: Text(planets[index]['name']!),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PlanetDetailsScreen(
-                      name: planets[index]['name']!,
-                      image: planets[index]['image']!,
-                      description: planets[index]['description']!,
-                    ),
-                  ),
-                );
-              },
+              leading: Image.network(item['image']),
+              title: Text(item['name']),
+              subtitle: Text(item['description']),
             ),
           );
         },
